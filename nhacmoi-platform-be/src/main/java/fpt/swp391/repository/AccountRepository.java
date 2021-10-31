1998bb5ecd3089ef572618e5055c94197561ca33
Nezumi <phanhuuloi15@gmail.com>
2021-10-31 17:59:42 +0700

    Log-in security, encrypt password

--- nhacmoi-platform-be/src/main/java/fpt/swp391/repository/AccountRepository.java
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package fpt.swp391.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fpt.swp391.model.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
-
+    @Query("select u from Account u where u.account_name = ?1 ")
+    Account findByAccountName(String accountName);
 }
