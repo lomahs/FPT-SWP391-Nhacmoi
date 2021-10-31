3ea6010fb730c9dd3bd9da2621b54a8d12817049
Nezumi <phanhuuloi15@gmail.com>
2021-10-31 18:00:14 +0700

    Log-in security, encrypt password

---
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/service/impl/CustomAccountDetailsService.java
@@ -0,0 +1,23 @@
+package fpt.swp391.service.impl;
+
+import fpt.swp391.model.Account;
+import fpt.swp391.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomAccountDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AccountRepository accountService;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountName) throws UsernameNotFoundException {
+        Account account = accountService.findByAccountName(accountName);
+        if(account == null){
+            throw new UsernameNotFoundException("Account not found");
+        }
+        return new CustomAccountDetails(account);
+    }
+}
