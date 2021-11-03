a91d6a555fb8715c56add1972a5576d8d271bacd
Nezumi <phanhuuloi15@gmail.com>
2021-11-03 23:20:39 +0700

    Email Verification

---
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/email/EmailSender.java
@@ -0,0 +1,6 @@
+package fpt.swp391.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
+
---
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/email/token/ConfirmationToken.java
@@ -0,0 +1,43 @@
+package fpt.swp391.email.token;
+
+import fpt.swp391.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "user_id"
+    )
+    private User user;
+
+    public ConfirmationToken(String token, LocalDateTime createAt, LocalDateTime expiresAt, User user) {
+        this.token = token;
+        this.createAt = createAt;
+        this.expiresAt = expiresAt;
+        this.user = user;
+    }
+}
