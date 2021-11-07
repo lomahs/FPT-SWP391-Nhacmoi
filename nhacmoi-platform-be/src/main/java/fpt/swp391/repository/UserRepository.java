b40df9cebf79022462702788dc05c02cc929019d
Lomahs <thanh.bui.bao@gmail.com>
2021-11-07 13:45:09 +0700

    Get Playlist based on token

--- nhacmoi-platform-be/src/main/java/fpt/swp391/repository/UserRepository.java
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/repository/UserRepository.java
@@ -2,8 +2,11 @@ package fpt.swp391.repository;
 
 import fpt.swp391.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
