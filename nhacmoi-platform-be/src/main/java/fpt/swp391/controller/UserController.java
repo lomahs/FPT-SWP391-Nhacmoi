23784673eed28c5412532b85bf1c1cba9fdd0213
Nezumi <phanhuuloi15@gmail.com>
2021-11-08 16:11:46 +0700

    Resend Email Confirm (Fix).

--- nhacmoi-platform-be/src/main/java/fpt/swp391/controller/UserController.java
+++ nhacmoi-platform-be/src/main/java/fpt/swp391/controller/UserController.java
@@ -114,8 +114,8 @@ public class UserController {
                 httpStatus = HttpStatus.BAD_REQUEST;
             }
         } catch (Exception ex) {
-            loginResponse = new LoginResponse(null, null, "User email is not confirmed.");
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            User user = userServices.getUserByAccountName(account.getAccount_name());
+            return userServices.resendEmailConfirm(user.getUser_email());
         }
         return new ResponseEntity<>(loginResponse, httpStatus);
     }
@@ -149,8 +149,8 @@ public class UserController {
         return userServices.updatePassword(emailResponse.getEmail(), emailResponse.getPassword(), emailResponse.getToken());
     }
 
-    @GetMapping(path = "/register/resend")
-    public ResponseEntity<LoginResponse> resendEmail(@RequestBody EmailResponse emailResponse){
-        return userServices.resendEmailConfirm(emailResponse.getEmail());
-    }
+//    @GetMapping(path = "/register/resend")
+//    public ResponseEntity<LoginResponse> resendEmail(@RequestBody EmailResponse emailResponse){
+//        return userServices.resendEmailConfirm(emailResponse.getEmail());
+//    }
 }
